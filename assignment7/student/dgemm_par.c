b9b3f9b92f6c60419c361885be9de389009bd12d
abeduplaa <abrahamduplaa@gmail.com>
2018-07-08 18:27:33 +0200

    Working on solution to assignment 7

--- assignment7/student/dgemm_par.c
+++ assignment7/student/dgemm_par.c
@@ -1,13 +1,47 @@
 #include"dgemm.h"
 #include <immintrin.h>
+#include <smmintrin.h>
+#include <string.h>
+
+
+void dotmul_intrins(float *A, float *B, float C, int SIZE)
+{
+
+  register __m128 *a, *b;
+  a = (__m128*) &A;
+  b = (__m128*) &B;
+
+  ///*
+
+  __m128 r1 = _mm_mul_ps(*a, *b);
+  __m128 r2 = _mm_hadd_ps(r1, r1);
+  __m128 r3 = _mm_hadd_ps(r2, r2);
+  _mm_store_ss(&C, r3);
+
+  //*/
+
+  //_mm_store_ss(C, _mm_hadd_ps(_mm_hadd_ps(_mm_mul_ps(*a, *b), _mm_mul_ps(*a, *b)), _mm_hadd_ps(_mm_mul_ps(*a, *b), _mm_mul_ps(*a, *b))));
+
+}
+
+
 
 void dgemm(float *a, float *b, float *c, int n)
 {
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < n; k++){
-                c[i * n + j] += a[i * n  + k] * b[j * n  + k];
-            }
-        }
+  float *a1, *b1;
+  a1 = (float*)malloc(n);
+  b1 = (float*)malloc(n);
+  for (int i = 0; i < n; ++i )
+  {
+    //float *a1 = &a[i * n];
+    memcpy(a1, &a[i*n], n*sizeof(*a));
+    for (int j = 0 ; j<n; ++j )
+    {
+      //b1 = &b[j*n];
+      memcpy(b1, &b[i*n], n*sizeof(*b));
+
+      dotmul_intrins(a1,b1,c[i * n + j],n);
+
     }
+  }
 }
---
+++ assignment7/student/dgemm_par.d
@@ -0,0 +1,4 @@
+student/dgemm_par.o: student/dgemm_par.c \
+ /media/psf/TUM_Courses/SS_18/PP/Tutorials/assignment7/dgemm.h
+
+/media/psf/TUM_Courses/SS_18/PP/Tutorials/assignment7/dgemm.h:
